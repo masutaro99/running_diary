8e0d180e1461b0624ac70202e028bd823fceb546
masutaro99 <maskenpa1001@gmail.com>
2021-01-23 12:46:14 +0900

    update: update index page to pick up db date

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,17 +1,17 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
   end
   def new
     @board = Board.new
   end
   def create
     Board.create(board_params)
-    binding.pry
   end
 
   private
 
   def board_params
-    params.require(:board).permit(:name, :titile, :body)
+    params.require(:board).permit(:name, :title, :body)
   end
 end

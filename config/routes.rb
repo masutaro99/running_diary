4115d08eb83ae7f44cd9913a7642f1820e428b3f
masutaro99 <maskenpa1001@gmail.com>
2021-01-23 21:49:35 +0900

    add: create edit page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "boards#index"
-  # get "boards" => "boards#index"
-  # get "boards/new" => "boards#new"
-  # post "boards" => "boards#create"
-  # get "boards/:id" => "boards#show"
-  resources :boards, only: [:index, :new, :create, :show]
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end

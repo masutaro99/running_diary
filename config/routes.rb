40b77bb0198fba51ff040dc6052b3afedbe80d18
masutaro99 <maskenpa1001@gmail.com>
2021-01-23 13:44:15 +0900

    update: update to use resource base routing and link_to method instead using a tag

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "boards#index"
-  get "boards" => "boards#index"
-  get "boards/new" => "boards#new"
-  post "boards" => "boards#create"
-  get "boards/:id" => "boards#show"
+  # get "boards" => "boards#index"
+  # get "boards/new" => "boards#new"
+  # post "boards" => "boards#create"
+  # get "boards/:id" => "boards#show"
+  resources :boards, only: [:index, :new, :create, :show]
 end

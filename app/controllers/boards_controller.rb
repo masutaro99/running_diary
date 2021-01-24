ddc230874b3068871866e138d84fd82ef1df19bd
masutaro99 <maskenpa1001@gmail.com>
2021-01-24 14:12:19 +0900

    refactor: integrate selecting board action to before_action:set_target_board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
     @boards = Board.all
   end
@@ -6,23 +8,19 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
   def create
-    board = Board.create(board_params)
-    redirect_to board
+    @board = Board.create(board_params)
+    redirect_to @board
   end
   def show
-    @board = Board.find(params[:id])
   end
   def edit
-    @board = Board.find(params[:id])
   end
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-    redirect_to board
+    @board.update(board_params)
+    redirect_to @board
   end
   def destroy
-    board = Board.find(params[:id])
-    board.delete
+    @board.delete
     redirect_to boards_path
   end
 
@@ -30,5 +28,8 @@ class BoardsController < ApplicationController
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end

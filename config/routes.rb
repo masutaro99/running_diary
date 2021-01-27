6f8a86d923a571501e029df8aceb0cd4feb3058e
masutaro99 <maskenpa1001@gmail.com>
2021-01-27 13:08:33 +0900

    add: add header and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "boards#index"
   resources :boards
   resources :comments, only: %i[create destroy]
 end
